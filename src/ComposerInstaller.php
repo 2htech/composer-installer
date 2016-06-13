a5c866e833b1a2bb6756410cc2691132dd22ac6a
AntikCz <antik494@gmail.com>
2016-06-13 15:27:11 +0200

    Refactoring

--- src/ComposerInstaller.php
+++ src/ComposerInstaller.php
@@ -12,26 +12,27 @@ use Composer\Util\Filesystem;
 
 class ComposerInstaller extends LibraryInstaller {
 
-	/** @var array */
-	private $types = [
-		"thunbolt-module" => "ins/modules",
-		"thunbolt-core" => "app",
-		"thunbolt-bin" => "bin",
-		"thunbolt-component" => "ins/components",
-		"thunbolt-package" => "ins/packages"
+	const PATHS = [
+		'appDir' => 'app',
+		'wwwDir' => 'www',
+		'binDir' => 'bin',
+		'insDir' => 'ins',
+		'cfgDir' => self::PATHS['appDir'] . '/config',
+		'modDir' => self::PATHS['appDir'] . '/modules',
+		'resourceFile' => self::PATHS['appDir'] . '/config/composer.json',
+		'resourceDir' => self::PATHS['wwwDir'] . '/ins-assets',
+		'binConfigs' => self::PATHS['bin'] . '/config.json',
+		'templateLoader' => self::PATHS['bin'] . '/loaders/template.json'
 	];
 
-	/** @var string */
-	protected $resourceDir = 'ins/assets';
-
-	/** @var string */
-	protected $resourceFile;
-
-	/** @var string */
-	protected $templateLoader = 'bin/loaders/template.json';
-
-	/** @var string */
-	protected $binConfigs = 'bin/config.json';
+	/** @var array */
+	private $types = array(
+		"thunbolt-module" => "ins/modules",
+		"thunbolt-bin" => "bin",
+		"thunbolt-component" => "ins/components",
+		"thunbolt-package" => "ins/packages",
+		"thunbolt-command" => "ins/commands"
+	);
 
 	/** @var array */
 	protected $jsonData = array();
@@ -39,49 +40,29 @@ class ComposerInstaller extends LibraryInstaller {
 	/** @var array */
 	protected $templateLoaderData = array();
 
-	/** @var string */
-	protected $appDir = 'app';
-
-	/** @var string */
-	protected $wwwDir = 'www';
-
-	/** @var string */
-	protected $configFile;
-
-	/**
-	 * Initializes library installer.
-	 *
-	 * @param IOInterface $io
-	 * @param Composer    $composer
-	 * @param string      $type
-	 * @param Filesystem  $filesystem
-	 */
-	public function __construct(IOInterface $io, Composer $composer, $type = 'library', Filesystem $filesystem = null) {
-		parent::__construct($io, $composer, $type, $filesystem);
-
-		$this->resourceFile = $this->appDir . '/config/composer.json';
-	}
-
 	/**
 	 * {@inheritDoc}
 	 */
 	public function getInstallPath(PackageInterface $package) {
-		if (in_array($package->getType(), array('webchemistry-core', 'webchemistry-bin'))) {
-			return $this->types[$package->getType()];
+		$type = $package->getType();
+		$extra = $package->getExtra();
+
+		if ($type === 'thunbolt-package' && isset($extra['packageName'])) {
+			return $this->types[$type] . '/' . $extra['packageName'];
 		}
 
-		return $this->types[$package->getType()]. '/' . $package->getPrettyName();
+		return $this->types[$type]. '/' . $package->getPrettyName();
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	protected function initialize() {
-		$this->filesystem->ensureDirectoryExists($this->appDir);
-		$this->filesystem->ensureDirectoryExists($this->wwwDir . '/' . $this->resourceDir);
-		$this->filesystem->ensureDirectoryExists($this->appDir . '/config');
+		$this->filesystem->ensureDirectoryExists(self::PATHS['appDir']);
+		$this->filesystem->ensureDirectoryExists(self::PATHS['resourceDir']);
+		$this->filesystem->ensureDirectoryExists(self::PATHS['cfgDir']);
 
-		$this->jsonData = file_exists($this->resourceFile) ? json_decode(file_get_contents($this->resourceFile), TRUE) : array();
+		$this->jsonData = file_exists(self::PATHS['resourceFile'] ? json_decode(file_get_contents(self::PATHS['resourceFile']), TRUE) : array();
 		if (!array_key_exists('assets', $this->jsonData)) {
 			$this->jsonData['assets'] = [];
 		}
@@ -89,7 +70,7 @@ class ComposerInstaller extends LibraryInstaller {
 			$this->jsonData['configs'] = [];
 		}
 
-		$this->templateLoaderData = file_exists($this->templateLoader) ? json_decode(file_get_contents($this->templateLoader), TRUE) : array();
+		$this->templateLoaderData = file_exists(self::PATHS['templateLoader']) ? json_decode(file_get_contents(self::PATHS['templateLoader']), TRUE) : array();
 	}
 
 	/**
@@ -97,8 +78,8 @@ class ComposerInstaller extends LibraryInstaller {
 	 */
 	protected function finish() {
 		array_unique($this->jsonData['assets']);
-		file_put_contents($this->resourceFile, json_encode($this->jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
-		file_put_contents($this->templateLoader, json_decode($this->templateLoader, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+		file_put_contents(self::PATHS['resourceFile'], json_encode($this->jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+		file_put_contents(self::PATHS['templateLoader'], json_decode($this->templateLoaderData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
 	}
 
 	/**
@@ -148,11 +129,15 @@ class ComposerInstaller extends LibraryInstaller {
 	}
 
 	/**
+	 * Only for "type": "thunbolt-bin"
+	 * extra: {
+	 * 		"configs": ["dir/config.neon"]
+	 * }
 	 * {@inheritDoc}
 	 */
 	protected function installTemplateLoader(PackageInterface $package) {
 		$extra = $package->getExtra();
-		if (!isset($extra['configs']) || $package->getType() !== 'webchemistry-bin') {
+		if (!isset($extra['configs']) || $package->getType() !== 'thunbolt-bin') {
 			return;
 		}
 
@@ -166,11 +151,15 @@ class ComposerInstaller extends LibraryInstaller {
 	}
 
 	/**
+	 * Only for "type": "thunbolt-bin"
+	 * extra: {
+	 * 		"configs": ["dir/config.neon"]
+	 * }
 	 * {@inheritDoc}
 	 */
 	protected function uninstallTemplateLoader(PackageInterface $package) {
 		$extra = $package->getExtra();
-		if (!isset($extra['configs']) || $package->getType() !== 'webchemistry-bin') {
+		if (!isset($extra['configs']) || $package->getType() !== 'thunbolt-bin') {
 			return;
 		}
 
@@ -182,11 +171,14 @@ class ComposerInstaller extends LibraryInstaller {
 	}
 
 	/**
+	 * extra: {
+	 * 		"configs": ["dir/config.neon"]
+	 * }
 	 * {@inheritDoc}
 	 */
 	protected function installConfigs(PackageInterface $package) {
 		$extra = $package->getExtra();
-		if (!isset($extra['configs']) || $package->getType() === 'webchemistry-bin') {
+		if (!isset($extra['configs']) || $package->getType() === 'thunbolt-bin') {
 			return;
 		}
 
@@ -200,11 +192,14 @@ class ComposerInstaller extends LibraryInstaller {
 	}
 
 	/**
+	 * extra: {
+	 * 		"configs": ["dir/config.neon"]
+	 * }
 	 * {@inheritDoc}
 	 */
 	protected function uninstallConfigs(PackageInterface $package) {
 		$extra = $package->getExtra();
-		if (!isset($extra['configs']) || $package->getType() === 'webchemistry-bin') {
+		if (!isset($extra['configs']) || $package->getType() === 'thunbolt-bin') {
 			return;
 		}
 
@@ -216,11 +211,18 @@ class ComposerInstaller extends LibraryInstaller {
 	}
 
 	/**
+	 * extra: {
+	 * 		"assets": {
+	 *			"css": ["file.css"]
+	 * 			"js": ["file.js"],
+	 * 			"other": ["image.png"]
+	 * 		}
+	 * }
 	 * {@inheritDoc}
 	 */
 	protected function uninstallAssets(PackageInterface $package) {
 		$extra = $package->getExtra();
-		if (!isset($extra['assets']['css']) && !isset($extra['assets']['js'])) {
+		if (!isset($extra['assets']['css']) && !isset($extra['assets']['js']) && !isset($extra['assets']['other'])) {
 			return;
 		}
 
@@ -228,6 +230,13 @@ class ComposerInstaller extends LibraryInstaller {
 	}
 
 	/**
+	 * extra: {
+	 * 		"assets": {
+	 *			"css": ["file.css"]
+	 * 			"js": ["file.js"],
+	 * 			"other": ["image.png"]
+	 * 		}
+	 * }
 	 * {@inheritDoc}
 	 */
 	protected function removeAssets(PackageInterface $package, array $extra) {
@@ -235,13 +244,20 @@ class ComposerInstaller extends LibraryInstaller {
 			return;
 		}
 
-		$target = $this->wwwDir . '/' . $this->resourceDir . '/' . str_replace('/', '_', $package->getName());
+		$target = self::PATHS['resourceDir'] . '/' . str_replace('/', '_', $package->getName());
 		$this->filesystem->removeDirectoryPhp($target);
 
 		unset($this->jsonData['assets']);
 	}
 
 	/**
+	 * extra: {
+	 * 		"assets": {
+	 *			"css": ["file.css"]
+	 * 			"js": ["file.js"],
+	 * 			"other": ["image.png"]
+	 * 		}
+	 * }
 	 * {@inheritDoc}
 	 */
 	protected function installAssets(PackageInterface $package) {
@@ -252,6 +268,7 @@ class ComposerInstaller extends LibraryInstaller {
 
 		$this->copyAssets('css', $package, $extra['assets']);
 		$this->copyAssets('js', $package, $extra['assets']);
+		$this->copyAssets('other', $package, $extra['assets']);
 	}
 
 	/**
@@ -264,22 +281,20 @@ class ComposerInstaller extends LibraryInstaller {
 
 		$this->jsonData['assets'][$package->getName()][$type] = [];
 
-		foreach ((array) $extra[$type] as $compiled => $assets) {
-			foreach ((array) $assets as $row) {
-				$rowPath = $this->getInstallPath($package) . '/' . $row;
-				if (!file_exists($rowPath)) {
-					$this->io->write("<warning>Skipped installation of '$row' for package " . $package->getName() . " file not found in package.</warning>");
-					continue;
-				}
-				$baseDir = $this->resourceDir . '/' . str_replace('/', '_', $package->getName()) . '/' . $type . '/' . basename($row);
-				$target = $this->wwwDir . '/' . $baseDir;
-				if (!file_exists($target)) {
-					$this->filesystem->ensureDirectoryExists(dirname($target));
-					file_put_contents($target, file_get_contents($rowPath));
-				}
-				$this->jsonData['assets'][$package->getName()][$type][$compiled][] = $baseDir;
+		foreach ((array) $extra[$type] as $row) {
+			$rowPath = $this->getInstallPath($package) . '/' . $row;
+			if (!file_exists($rowPath)) {
+				$this->io->write("<warning>Skipped installation of '$row' for package " . $package->getName() . " file not found in package.</warning>");
+				continue;
 			}
+			$target = self::PATHS['resourceDir'] . '/' . str_replace('/', '_', $package->getName()) . '/' . $type . '/' . basename($row);
+			if (!file_exists($target)) {
+				$this->filesystem->ensureDirectoryExists(dirname($target));
+				copy($target, $rowPath);
+			}
+			$this->jsonData['assets'][$package->getName()][$type][] = $baseDir;
 		}
+
 	}
 
 	/**
