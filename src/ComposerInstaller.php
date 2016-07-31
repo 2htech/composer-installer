28866e03bbb7d4703b9ec401cc69071327c25504
MartkCz <antik494@gmail.com>
2016-07-31 17:54:23 +0200

    Simplier installer

--- src/ComposerInstaller.php
+++ src/ComposerInstaller.php
@@ -13,25 +13,17 @@ use Composer\Util\Filesystem;
 class ComposerInstaller extends LibraryInstaller {
 
 	const PATHS = [
-		'appDir' => 'app',
 		'wwwDir' => 'www',
-		'binDir' => 'bin',
-		'insDir' => 'mod',
-		'cfgDir' => 'app/config',
-		'modDir' => 'app/modules',
-		'resourceFile' => 'app/config/composer.json',
+		'modDir' => 'mod',
 		'resourceDir' => 'www/mod-assets',
-		'binConfigs' => 'bin/config.json',
-		'templateLoader' => 'bin/loaders/template.json'
+		'resourceFile' => 'mod/composer.json',
 	];
 
 	/** @var array */
 	private $types = array(
 		"thunbolt-module" => "mod/modules",
-		"thunbolt-bin" => "bin",
 		"thunbolt-component" => "mod/components",
-		"thunbolt-package" => "mod/packages",
-		"thunbolt-command" => "mod/commands"
+		"thunbolt-package" => "mod/packages"
 	);
 
 	/** @var array */
@@ -41,26 +33,15 @@ class ComposerInstaller extends LibraryInstaller {
 	 * {@inheritDoc}
 	 */
 	public function getInstallPath(PackageInterface $package) {
-		$type = $package->getType();
-		$extra = $package->getExtra();
-
-		if ($type === 'thunbolt-bin') {
-			return $this->types[$type] . '/' . (isset($extra['packageName']) ? $extra['packageName'] : 'vendor/' . $package->getPrettyName());
-		}
-		if ($type === 'thunbolt-package' && isset($extra['packageName'])) {
-			return $this->types[$type] . '/' . $extra['packageName'];
-		}
-
-		return $this->types[$type]. '/' . $package->getPrettyName();
+		return $this->types[$package->getType()]. '/' . $package->getPrettyName();
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	protected function initialize() {
-		$this->filesystem->ensureDirectoryExists(self::PATHS['appDir']);
+		$this->filesystem->ensureDirectoryExists(self::PATHS['modDir']);
 		$this->filesystem->ensureDirectoryExists(self::PATHS['resourceDir']);
-		$this->filesystem->ensureDirectoryExists(self::PATHS['cfgDir']);
 
 		$this->jsonData = file_exists(self::PATHS['resourceFile']) ? json_decode(file_get_contents(self::PATHS['resourceFile']), TRUE) : array();
 		if (!array_key_exists('configs', $this->jsonData)) {
@@ -85,7 +66,6 @@ class ComposerInstaller extends LibraryInstaller {
 		parent::install($repo, $package);
 
 		$this->initialize();
-		$this->installBin($package);
 		$this->installConfigs($package);
 		$this->installAssets($package);
 		$this->finish();
@@ -97,8 +77,6 @@ class ComposerInstaller extends LibraryInstaller {
 	public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target) {
 		if ($this->isInstalled($repo, $initial)) {
 			$this->initialize();
-			$this->uninstallBin($initial);
-			$this->installBin($target);
 			$this->uninstallConfigs($initial);
 			$this->installConfigs($target);
 			$this->uninstallAssets($initial);
@@ -115,86 +93,12 @@ class ComposerInstaller extends LibraryInstaller {
 	public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package) {
 		if ($this->isInstalled($repo, $package)) {
 			$this->initialize();
-			$this->uninstallBin($package);
 			$this->uninstallConfigs($package);
 			$this->uninstallAssets($package);
 			$this->finish();
 		}
 
 		parent::uninstall($repo, $package);
-	}
-
-	/**
-	 * Only for "type": "thunbolt-bin"
-	 * extra: {
-	 * 		"config": ["config.json"],
-	 * 		"bin": ["dir/bin.bat"]
-	 * }
-	 * {@inheritDoc}
-	 */
-	protected function installBin(PackageInterface $package) {
-		$extra = $package->getExtra();
-		if ($package->getType() !== 'thunbolt-bin') {
-			return;
-		}
-
-		if (isset($extra['config'])) {
-			foreach ((array) $extra['config'] as $config) {
-				$this->filesystem->ensureDirectoryExists(self::PATHS['binDir'] . '/config');
-				if (!file_exists($this->getInstallPath($package) . '/' . $config)) {
-					$this->io->write("<warning>Skipped installation of '$config' for package " . $package->getName() . " file not found in package.</warning>");
-				}
-
-				if (!copy($this->getInstallPath($package) . '/' . $config, self::PATHS['binDir'] . '/config/' . basename($config))) {
-					$this->io->write("<warning>Skipped installation of '$config' for package " . $package->getName() . " file cannot copy to target directory.</warning>");
-				}
-			}
-		}
-		if (isset($extra['bin'])) {
-			foreach ((array) $extra['bin'] as $bin) {
-				if (!file_exists($this->getInstallPath($package) . '/' . $bin)) {
-					$this->io->write("<warning>Skipped installation of '$bin' for package " . $package->getName() . " file not found in package.</warning>");
-				}
-
-				if (!copy($this->getInstallPath($package) . '/' . $bin, self::PATHS['binDir'] . '/' . basename($bin))) {
-					$this->io->write("<warning>Skipped installation of '$bin' for package " . $package->getName() . " file cannot copy to target directory.</warning>");
-				}
-			}
-		}
-	}
-
-	/**
-	 * Only for "type": "thunbolt-bin"
-	 * extra: {
-	 * 		"config": ["dir/config.json"],
-	 * 		"bin": ["dir/bin.bat"]
-	 * }
-	 * {@inheritDoc}
-	 */
-	protected function uninstallBin(PackageInterface $package) {
-		$extra = $package->getExtra();
-		if ($package->getType() !== 'thunbolt-bin') {
-			return;
-		}
-
-		if (isset($extra['config'])) {
-			foreach ($extra['config'] as $config) {
-				if (file_exists($path = self::PATHS['binDir'] . '/' . basename($config))) {
-					$this->filesystem->unlink($path);
-					if ($this->filesystem->isDirEmpty(dirname($path))) {
-						$this->filesystem->removeDirectory(dirname($path));
-					}
-				}
-			}
-		}
-
-		if (isset($extra['bin'])) {
-			foreach ($extra['bin'] as $bin) {
-				if (file_exists($path = self::PATHS['binDir'] . '/' . $bin)) {
-					$this->filesystem->unlink($path);
-				}
-			}
-		}
 	}
 
 	/**
@@ -205,7 +109,7 @@ class ComposerInstaller extends LibraryInstaller {
 	 */
 	protected function installConfigs(PackageInterface $package) {
 		$extra = $package->getExtra();
-		if (!isset($extra['configs']) || $package->getType() === 'thunbolt-bin') {
+		if (!isset($extra['configs'])) {
 			return;
 		}
 
@@ -226,7 +130,7 @@ class ComposerInstaller extends LibraryInstaller {
 	 */
 	protected function uninstallConfigs(PackageInterface $package) {
 		$extra = $package->getExtra();
-		if (!isset($extra['configs']) || $package->getType() === 'thunbolt-bin') {
+		if (!isset($extra['configs'])) {
 			return;
 		}
 
@@ -259,7 +163,7 @@ class ComposerInstaller extends LibraryInstaller {
 	 * {@inheritDoc}
 	 */
 	protected function removeAssets(PackageInterface $package, array $extra) {
-		$target = self::PATHS['resourceDir'] . '/' . str_replace('/', '_', $package->getName());
+		$target = self::PATHS['resourceDir'] . '/' . $package->getName();
 		$this->filesystem->removeDirectoryPhp($target);
 	}
 
@@ -304,7 +208,7 @@ class ComposerInstaller extends LibraryInstaller {
 			$this->io->write("<warning>Skipped installation of '$absolutePath' for package " . $package->getName() . " file not found in package.</warning>");
 			return;
 		}
-		$target = self::PATHS['resourceDir'] . '/' . str_replace('/', '_', $package->getName()) . '/' . ltrim(str_replace($this->getInstallPath($package), '', $absolutePath), '\\/');
+		$target = self::PATHS['resourceDir'] . '/' . $package->getName() . '/' . ltrim(str_replace($this->getInstallPath($package), '', $absolutePath), '\\/');
 		if (!file_exists($target)) {
 			$this->filesystem->ensureDirectoryExists(dirname($target));
 			copy($absolutePath, $target);
