8f4eee5a2776f08e92bf8c0bfb130c960bdf4281
MartkCz <antik494@gmail.com>
2017-07-09 17:29:05 +0200

    fixed paths of assets

--- src/ComposerDirectories.php
+++ src/ComposerDirectories.php
@@ -5,7 +5,7 @@ namespace Thunbolt\Composer;
 class ComposerDirectories {
 
 	const PLUGIN_DIR = 'plugins',
-		  PLUGIN_ASSETS_PATH = 'www/plugins-assets',
-		  PLUGIN_ASSETS_DIR = 'plugins-assets';
+		PLUGIN_ASSETS_PATH = 'www/plugin-assets',
+		PLUGIN_ASSETS_DIR = 'plugin-assets';
 
 }
