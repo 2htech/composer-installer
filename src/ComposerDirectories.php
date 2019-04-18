ad3e07c06799b2e8daf6fa6284c13126306130c1
Tomáš Haubert <haubert@2htech.cz>
2019-04-18 15:35:44 +0200

    Update ComposerDirectories.php

--- src/ComposerDirectories.php
+++ src/ComposerDirectories.php
@@ -5,7 +5,7 @@ namespace App2h\Composer;
 class ComposerDirectories {
 
 	const MODULE_DIR = 'modules',
-		MODULE_ASSETS_PATH = 'www/modules-assets',
+		MODULE_ASSETS_PATH = 'www/module-assets',
 		MODULE_ASSETS_DIR = 'module-assets';
 
 }
